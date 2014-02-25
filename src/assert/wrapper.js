48b5857078739fa0c1346fd43bf275c5ebccfb44
Alex Kit <source.kit@gmail.com>
2014-02-25 12:58:36 +0100

    use assertion events, instead of fn wrapper

--- src/assert/wrapper.js
+++ src/assert/wrapper.js
@@ -1,6 +1,6 @@
 
 
-global.assert = assert = wrapAssertFns(wrapAssert(assert));
+global.assert = assert;
 
 obj_extend(assert, {
 	total: 0,
@@ -11,11 +11,11 @@ obj_extend(assert, {
 	reset: function(){
 		
 		this.callbacks.length = 0;
+		this.timeouts.length = 0;
+		
 		this.failed = 0;
 		this.total = 0;
 		this.errors = 0;
-		
-		this.timeouts = [];
 	},
 	
 	// @obsolete
@@ -23,6 +23,31 @@ obj_extend(assert, {
 		return this.await(callback);
 	}
 });
+
+//> events
+(function(assert){
+	
+	assert.on('start', function(){
+		assert.total++;
+	});
+	
+	assert.on('success', function(){
+		
+		assert.onsuccess && assert.onsuccess();
+	});
+	
+	assert.on('error', function(error){
+		
+		// force stack calculation
+		// (otherwise stack is not sent from browsers to the server)
+		error.stack = error.stack;
+		
+		
+		assert.failed++;
+		assert.onfailure && assert.onfailure(error);
+	});
+	
+}(assert));
 
 /** GLOBALS */
 global.eq = assert.equal;
@@ -40,56 +65,3 @@ function obj_extend(target, source) {
 	}
 	return target;
 }
-
-function wrapAssert(original) {
-	var fn;
-	fn = wrapFn(original);
-	fn.prototype = original.prototype;
-	
-	return obj_extend(fn, original);
-}
-
-function wrapAssertFns(assert) {
-		
-	for (var key in assert) {
-		if (typeof assert[key] !== 'function') 
-			continue;
-		
-		if (key === 'await') 
-			continue;
-		
-		if (key[0].toLowerCase() !== key[0]) {
-			// Class Function Definition
-			continue;
-		}
-	
-		assert[key] = wrapFn(assert, key);
-	}
-	return assert;
-}
-
-function wrapFn(assertFn, key) {
-	var original = key == null ? assertFn : assertFn[key];
-	
-	return function(){
-		assert.total++;
-		try {
-			original.apply(this, arguments);
-		} catch(error) {
-			assert.failed++;
-			
-			// force stack calculation
-			// (otherwise stack is not sent from browsers to the server)
-			error.stack = error.stack;
-			error.file = error.file;
-			error.line = error.line;
-			
-			assert.onfailure && assert.onfailure(error);
-			return;
-		}
-		
-		assert.onsuccess && assert.onsuccess();
-	};
-}
-
-
