eae6b5225b47c7a18637fffd94023d4da3fc3f41
Alex Kit <source.kit@gmail.com>
2014-04-25 16:53:53 +0200

    + transport jam: document readyState

--- src/utils/transport.jam.js
+++ src/utils/transport.jam.js
@@ -17,12 +17,34 @@ var transport_isBusy;
 	function dom(){
 		
 		transport_isBusy = function(win){
-			return xhr_isBusy(win);
+			return xhr_isBusy(win) || dom_isReady(win) === false;
 		};
 		
 		//= private
 		// Initialize
 		xhr_createListener(window);
+	}
+	
+	
+	function dom_isReady(win){
+		if (frame_isReady(win) === false) 
+			return false;
 		
+		var i = win.frames.length;
+		while ( --i > -1) {
+			if (frame_isReady(win.frames[i]) === false) 
+				return false;
+		}
+		
+		function frame_isReady(frame){
+			try {
+				return frame.document.readyState === 'complete';
+			} catch(error){
+				// cross-origin ?
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }());
