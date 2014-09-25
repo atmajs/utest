7881bdbf3b7fda1f7a0888cbc9c501b60627e901
Alex Kit <source.kit@gmail.com>
2014-09-25 17:04:19 +0200

    + jQuery DocumentFragment patch

--- src/patch/jquery.js
+++ src/patch/jquery.js
@@ -2,16 +2,9 @@
 	/* Make jQuery to support DocumentFragment`s */
 	
 	var _jQuery = global.jQuery;
+	_jQuery.noConflict();
 	
-	function Wrapper(){};
-	Wrapper.prototype = $.fn;
-	
-	for(var key in _jQuery) {
-		Wrapper[key] = _jQuery[key];
-	}
-	
-	
-	global.$ = function(frag){
+	function Wrapper (frag) {
 		var args = Array.prototype.slice.call(arguments);
 		if (frag != null && typeof frag === 'object' && frag.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
 			args[0] = frag.children;
@@ -19,4 +12,11 @@
 		
 		return _jQuery.apply(global, args);
 	};
+	Wrapper.prototype = _jQuery.prototype;
+	
+	for(var key in _jQuery) {
+		Wrapper[key] = _jQuery[key];
+	}
+	
+	global.$ = Wrapper;
 }());
