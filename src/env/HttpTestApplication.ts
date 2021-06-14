4b100c2479bcea870d93c11313c7157664633b4f
Alex Kit <alex.kit@atmajs.com>
2021-06-14 22:30:46 +0200

    publish: bump

--- src/env/HttpTestApplication.ts
+++ src/env/HttpTestApplication.ts
@@ -4,35 +4,35 @@ import { class_Uri } from 'atma-utils';
 declare var require;
 
 var resume = include.pause(),
-	server = atma.server,
-	appConfig = app.config,
-	bodyParser = require('body-parser'),
-	Url = require('url')
+    server = atma.server,
+    appConfig = app.config,
+    bodyParser = require('body-parser'),
+    Url = require('url')
     ;
 
 
 server
-.Application
-.create({
-	base: include.location,
-	configs: class_Uri.combine(include.location, 'server/config/**.yml')
-})
-.then((app) => {
+    .Application
+    .create({
+        base: include.location,
+        configs: class_Uri.combine(include.location, 'server/config/**.yml')
+    })
+    .then((app) => {
 
-	app.responders([
-		function(req, res, next){
+        app.responders([
+            function(req, res, next){
 
-			io.File.disableCache();
-			server.StaticContent.Cache.state(false);
+                io.File.disableCache();
+                server.StaticContent.Cache.state(false);
 
-			var url = Url.parse(req.url, true);
-			req.query = url.query;
-			next();
-		},
-		bodyParser.json(),
-		app.responder(),
-		atma.server.StaticContent.respond
-	]);
+                var url = Url.parse(req.url, true);
+                req.query = url.query;
+                next();
+            },
+            bodyParser.json(),
+            app.responder(),
+            atma.server.StaticContent.respond
+        ]);
 
-	resume(app);
-});
+        resume(app);
+    });
